4622ab936e409ff7a43fb2201cca07de8b0ebcaa
mrjones-plip <mrjones-plip@plip.com>
2023-01-10 20:35:21 -0800

    WIP

--- conf.example.py
+++ conf.example.py
@@ -2,4 +2,5 @@ trackme = {
     '10.220.249.232': ["mrjones"],
 }
 ssh_user = 'timekpr-next-remote'
+ssh_password = 'timekpr-next-remote'
 ssh_timekpra_bin = '/usr/bin/timekpra'
--- main.py
+++ main.py
@@ -6,9 +6,20 @@ from fabric import Connection
 
 
 def get_usage(user, computer):
+    #
+    connect_kwargs = {
+        "key_filename": "./id_timekpr",
+        "password": conf.ssh_password
+    }
     # todo - put this in try/except for when the command fails
-    timekpra_userinfo_output = Connection(computer, user=conf.ssh_user).\
-        run(conf.ssh_timekpra_bin + ' --userinfo ' + user, hide=True)
+    timekpra_userinfo_output = Connection(
+        host=computer,
+        user=conf.ssh_user,
+        connect_kwargs=connect_kwargs
+    ).run(
+        conf.ssh_timekpra_bin + ' --userinfo ' + user,
+        hide=True
+    )
 
     # todo - remove this sample output
     # ACTUAL_TIME_SPENT_DAY: 0
@@ -16,11 +27,13 @@ def get_usage(user, computer):
     # ACTUAL_TIME_LEFT_CONTINUOUS: 3600
     # ACTUAL_PLAYTIME_LEFT_DAY: 0
 
-    search = r"(ACTUAL_TIME_LEFT_DAY: )(\d{4})"
+    search = r"(TIME_LEFT_DAY: )(\d{4})"
     actual_time_left = re.search(search, str(timekpra_userinfo_output))
-    # todo - handle "else" when we can't find time remaining
+    # todo - better handle "else" when we can't find time remaining
     if actual_time_left and actual_time_left.group(2):
-        print(f"time left for {user} at {computer}: " + str(actual_time_left.group(2)) )
+        print(f"time left for {user} at {computer}: " + str(actual_time_left.group(2)))
+    else:
+        print(f"error! Could not get get usage for {user} at {computer}" + str(actual_time_left) + conf.ssh_user + computer )
 
 
 def main():
