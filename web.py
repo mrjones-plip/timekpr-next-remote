37dfb4f163dfc4043a4ed8bc41624d94983342fb
mrjones-plip <mrjones-plip@plip.com>
2023-01-12 22:26:50 -0800

    WIP

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import main
 import conf, re
 from fabric import Connection
 from paramiko.ssh_exception import AuthenticationException
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,9 +10,35 @@ app = Flask(__name__)
 def config():
     return main.get_config()
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/get_usage/<computer>/<user>")
+def get_usage(computer, user):
+    # todo - validate computer and user exist in config
+    ssh = main.get_connection(computer)
+    return main.get_usage(user, computer, ssh)
+
+
+@app.route("/increase_time/<computer>/<user>/<seconds>")
+def increase_time(computer, user, seconds):
+    # todo - validate computer and user exist in config
+    ssh = main.get_connection(computer)
+    main.increase_time(seconds, ssh, user)
+    return "done"
+
+
+@app.route("/decrease_time/<computer>/<user>/<seconds>")
+def decrease_time(computer, user, seconds):
+    # todo - validate computer and user exist in config
+    ssh = main.get_connection(computer)
+    main.decrease_time(seconds, ssh, user)
+    return "done"
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
